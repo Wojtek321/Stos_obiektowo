b16380ad487121777117a638691027c41bca614c
Wojciech Domański <wojtekdxd@gmail.com>
2023-03-09 18:23:19 +0100

    Stos

---
+++ Stos/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Stos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stos stos1 = new Stos();
+            Stos stos2 = new Stos();
+            stos2.Init(10);
+            stos1.Init(10);
+
+            int liczba;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Wprowadz {i+1}. liczbe do stosu: ");
+                liczba = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                stos1.push(liczba);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                liczba = stos1.top();
+                stos2.push(liczba);
+                stos1.pop();
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(stos2.top());
+                liczba = stos2.top();
+                stos1.push(liczba);
+                stos2.pop();
+            }
+
+        }
+    }
+
+    class Stos
+    {
+        private int size;
+        private int count;
+        private int[] array;
+
+        public void Init(int size)
+        {
+            this.count = 0;
+            this.size = size;
+            this.array = new int[size];
+        }
+        
+        public bool empty()
+        {
+            return this.count == 0;
+        }
+
+        public bool full()
+        {
+            return this.count == size;
+        }
+
+        public void push(int n)
+        {
+            if (!full())
+            {
+                this.array[count] = n;
+                this.count++;
+            }
+            else
+            {
+                Console.WriteLine("Stos jest pelny.");
+            }
+                
+        }
+
+        public void pop()
+        {
+            if (!empty())
+            {
+                this.array[count - 1] = 0;
+                this.count--;
+            }
+            else
+            {
+                Console.WriteLine("Stos jest pusty");
+            }
+            
+        }
+
+        public int top()
+        {
+            if (!empty())
+            {
+                return this.array[count - 1];
+            }
+            else
+            {
+                return int.MinValue;
+            }
+        }
+    }
+}
